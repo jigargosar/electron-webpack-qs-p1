72cc7d19e8c9ea691dd124f6d98fb5c04d7a954b
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-26 19:05:57 +0530

    useAppModel

---
+++ src/renderer/useAppModel.js
@@ -0,0 +1,79 @@
+import * as R from 'ramda'
+import PouchDb from 'pouchdb-browser'
+import * as nanoid from 'nanoid'
+import faker from 'faker'
+import { useEffect, useMemo, useState } from 'react'
+
+const db = new PouchDb('notes')
+
+function createNewNote() {
+  return {
+    _id: nanoid(),
+    content: faker.lorem.paragraph(),
+    createdAt: Date.now(),
+    modifiedAt: Date.now(),
+  }
+}
+
+function addNewNote(setModel) {
+  const newNote = createNewNote()
+  db.put(newNote).catch(e => setModel(handleNotesDbError(e)))
+}
+
+function handleNotesDbChange(change) {
+  return model => {
+    if (change.deleted) {
+      return R.dissocPath(['notesById', change.id])(model)
+    } else {
+      const doc = change.doc
+      return R.assocPath(['notesById', doc._id])(doc)(model)
+    }
+  }
+}
+
+function handleNotesDbError(err) {
+  console.error('handleNotesDbError', err)
+  return R.assoc('lastErrMsg')(err.message)
+}
+
+function useLogModelEffect(model) {
+  useEffect(() => console.table(R.values(model.notesById)), [model])
+}
+
+function usePouchNotesEffect(setModel) {
+  useEffect(() => {
+    const changes = db
+      .changes({ include_docs: true, live: true })
+      .on('change', change => setModel(handleNotesDbChange(change)))
+      .on('error', err => setModel(handleNotesDbError(err)))
+    return () => changes.cancel()
+  }, [])
+}
+
+function getAllNotes(model) {
+  R.values(model.notesById)
+}
+
+export function getDisplayNotes(model) {
+  return R.compose(
+    R.sortWith([R.descend(R.prop('modifiedAt'))]),
+    R.values,
+  )(model.notesById)
+}
+
+export function useAppModel() {
+  const [model, setModel] = useState({ notesById: {}, lastErrMsg: null })
+
+  useLogModelEffect(model)
+  usePouchNotesEffect(setModel)
+
+  const actions = useMemo(
+    () => ({
+      onAddClicked: () => addNewNote(setModel),
+      onNoteListHeadingClick: () => console.table(getAllNotes(model)),
+    }),
+    [],
+  )
+
+  return [model, actions]
+}
