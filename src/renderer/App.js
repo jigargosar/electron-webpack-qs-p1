087a0336b7a70f45150dee9f248afec5b88399d7
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-26 19:06:42 +0530

    useAppModel

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="sans-serif lh-title measure center">
-      <div className="f4" onClick={actions.onNoteListHeadingClick}>
+      <div className="f4 pv2" onClick={actions.onNoteListHeadingClick}>
         Notes List
       </div>
       <button onClick={actions.onAddClicked}>add new</button>
