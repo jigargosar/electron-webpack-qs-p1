0c08022c4e6ba43c4c0a04f55935c6893003f4d4
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-26 15:20:46 +0530

    init

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
-import {app, BrowserWindow} from 'electron'
+import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
-import {format as formatUrl} from 'url'
+import { format as formatUrl } from 'url'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -10,21 +10,26 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 let mainWindow
 
 function createMainWindow() {
-  const window = new BrowserWindow({webPreferences:{nodeIntegration: true}})
+  const window = new BrowserWindow({
+    webPreferences: { nodeIntegration: true },
+  })
 
   if (isDevelopment) {
     window.webContents.openDevTools()
   }
 
   if (isDevelopment) {
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
-  }
-  else {
-    window.loadURL(formatUrl({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file',
-      slashes: true
-    }))
+    window.loadURL(
+      `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`,
+    )
+  } else {
+    window.loadURL(
+      formatUrl({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true,
+      }),
+    )
   }
 
   window.on('closed', () => {
