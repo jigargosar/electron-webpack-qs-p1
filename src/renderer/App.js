c9d5c7b5fdc7796c8e19ccec2aabc2604b171d4c
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-26 18:15:34 +0530

    Notes

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import * as R from 'ramda'
-import { observer, useObservable } from 'mobx-react-lite'
+import * as nanoid from 'nanoid'
+import faker from 'faker'
 
 // const db = new PouchDb('notes')
 
+function createNewNote() {
+  return {
+    _id: nanoid(),
+    content: faker.lorem.paragraph(),
+    createdAt: Date.now(),
+    modifiedAt: Date.now(),
+  }
+}
+
+function addNewNote(model) {
+  const newNote = createNewNote()
+  model.foo = 1
+  return R.compose(R.assocPath(['notesById', newNote._id])(newNote))(model)
+}
+
 function App() {
-  const model = useObservable({ notesById: {} })
+  const [model, setModel] = useState({ notesById: {} })
+
+  useEffect(() => console.log(model), [model])
 
   return (
     <div className="sans-serif lh-title measure center">
       <div className="f4">Notes List</div>
+      <button onClick={() => setModel(addNewNote)}>add new</button>
       {R.values(model.notesById).map(note => (
         <div key={note._id} className="pa2">
           {note.content}
@@ -19,4 +38,4 @@ function App() {
   )
 }
 
-export default observer(App)
+export default App
