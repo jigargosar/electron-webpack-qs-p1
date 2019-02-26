a9a0ed26782c0ad59edbbf5804723e7400b35445
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-26 18:04:36 +0530

    Notes

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,53 +1,18 @@
 import React from 'react'
-import { clipboard } from 'electron'
 import * as R from 'ramda'
-import { now } from 'mobx-utils'
-import { observer, useDisposable, useObservable } from 'mobx-react-lite'
-import { reaction } from 'mobx'
+import { observer, useObservable } from 'mobx-react-lite'
 
 // const db = new PouchDb('notes')
 
-function getClipText() {
-  return clipboard.readText()
-}
-
-function prependNewClipItem(next) {
-  return R.compose(
-    R.uniq,
-    R.take(10),
-    R.prepend(R.compose(R.take(1024))(next)),
-  )
-}
-
-function addClipTextIfNew(clip) {
-  const next = getClipText()
-  if (!R.equals(next, clip.prev)) {
-    console.log(`next`, next)
-    console.log(`prev`, clip.prev)
-    clip.prev = next
-    clip.buff = prependNewClipItem(next)(clip.buff)
-  }
-}
-
 function App() {
-  const clip = useObservable({ prev: null, buff: [] })
-
-  useDisposable(() =>
-    reaction(
-      () => [now()],
-      () => {
-        addClipTextIfNew(clip)
-      },
-      { fireImmediately: true },
-    ),
-  )
+  const model = useObservable({ notesById: {} })
 
   return (
     <div className="sans-serif lh-title measure center">
-      <div className="f4">Clipboard History</div>
-      {clip.buff.map((clipText, idx) => (
-        <div key={idx} className="pa2">
-          {clipText}
+      <div className="f4">Notes List</div>
+      {R.values(model.notesById).map(note => (
+        <div key={note._id} className="pa2">
+          {note.content}
         </div>
       ))}
     </div>
---
+++ src/renderer/ClipApp.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { clipboard } from 'electron'
+import * as R from 'ramda'
+import { now } from 'mobx-utils'
+import { observer, useDisposable, useObservable } from 'mobx-react-lite'
+import { reaction } from 'mobx'
+
+// const db = new PouchDb('notes')
+
+function getClipText() {
+  return clipboard.readText()
+}
+
+function prependNewClipItem(next) {
+  return R.compose(
+    R.uniq,
+    R.take(10),
+    R.prepend(R.compose(R.take(1024))(next)),
+  )
+}
+
+function addClipTextIfNew(clip) {
+  const next = getClipText()
+  if (!R.equals(next, clip.prev)) {
+    console.log(`next`, next)
+    console.log(`prev`, clip.prev)
+    clip.prev = next
+    clip.buff = prependNewClipItem(next)(clip.buff)
+  }
+}
+
+function ClipApp() {
+  const clip = useObservable({ prev: null, buff: [] })
+
+  useDisposable(() =>
+    reaction(
+      () => [now()],
+      () => {
+        addClipTextIfNew(clip)
+      },
+      { fireImmediately: true },
+    ),
+  )
+
+  return (
+    <div className="sans-serif lh-title measure center">
+      <div className="f4">Clipboard History</div>
+      {clip.buff.map((clipText, idx) => (
+        <div key={idx} className="pa2">
+          {clipText}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default observer(ClipApp)
