917040885c0c4f585748c4112f93f8ad23cafb6d
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-26 18:59:42 +0530

    Notes Pouch

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -58,23 +58,31 @@ function usePouchNotesEffect(setModel) {
   }, [])
 }
 
-function App() {
+function getAllNotes(model) {
+  R.values(model.notesById)
+}
+
+function useAppModel() {
   const [model, setModel] = useState({ notesById: {}, lastErrMsg: null })
 
   useLogModelEffect(model)
   usePouchNotesEffect(setModel)
 
   const onAddClicked = () => addNewNote(setModel)
+  const onNoteListHeadingClick = () => console.table(getAllNotes(model))
+
+  return [model, { onAddClicked, onNoteListHeadingClick }]
+}
+
+function App() {
+  const [model, actions] = useAppModel()
 
   return (
     <div className="sans-serif lh-title measure center">
-      <div
-        className="f4"
-        onClick={() => console.table(R.values(model.notesById))}
-      >
+      <div className="f4" onClick={actions.onNoteListHeadingClick}>
         Notes List
       </div>
-      <button onClick={onAddClicked}>add new</button>
+      <button onClick={actions.onAddClicked}>add new</button>
       {getDisplayNotes(model).map(note => (
         <NoteItem key={note._id} note={note} />
       ))}
