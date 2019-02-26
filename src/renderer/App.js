d6c0c498ecf5b2f27e7db4bf4a968dac4145cae5
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-26 17:11:06 +0530

    Notes

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import fs from 'fs'
 import PouchDb from 'pouchdb-browser'
-
-const electron = require('electron')
-const app = electron.app || electron.remote.app
 
 const db = new PouchDb('notes')
 
 export default function App() {
-  const codeSnippet = getCodeSnippet()
   return <div className="sans-serif lh-title">Notes</div>
 }
 
@@ -16,17 +11,4 @@ if (module.hot) {
   module.hot.dispose(() => {
     console.clear()
   })
-}
-
-window.electron = electron
-window._fs = fs
-window.app = app
-
-const clipboard = electron.clipboard
-
-function getCodeSnippet() {
-  return _fs.readFileSync(
-    '/Users/jigargosar/dev/electron-webpack-qs-p1/src/renderer/App.js',
-    { encoding: 'UTF-8' },
-  )
 }
