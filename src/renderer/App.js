a9b98d45ee726682dc53bdcd63c52ec8e8aa9605
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-26 15:44:22 +0530

    electron info

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -3,12 +3,28 @@ import React from 'react'
 const electron = require('electron')
 const app = electron.app || electron.remote.app
 
+window.electron = electron
+
 export default function App() {
   return (
     <div className="sans-serif lh-title">
       <div className="f4">Electron Info</div>
       <div className="">userData: {app.getPath('userData')}</div>
       <div className="">cwd: {process.cwd()}</div>
+      <pre>
+        <pre
+          style={{
+            'background-color': '#ffffff',
+            color: '#000000',
+            fontFamily: 'Menlo',
+            fontSize: '9.0pt',
+          }}
+        >
+          <span style={{ color: '#008000', fontWeight: 'bold' }}>
+            userData
+          </span>
+        </pre>
+      </pre>
     </div>
   )
 }
